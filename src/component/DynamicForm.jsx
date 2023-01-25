de6f294bf10c7d2be73242daf577efbd190c0b7f
rohitthakur211 <112896810+rohitthakur211@users.noreply.github.com>
2023-01-25 19:16:57 +0530

    form

--- src/component/DynamicForm.jsx
+++ src/component/DynamicForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Checkbox, TextField, Select, Grid } from "@mui/material";
+import { TextField, Select, Grid } from "@mui/material";
 
 function DynamicForm(props) {
   const { data } = props;
@@ -9,24 +9,78 @@ function DynamicForm(props) {
     <form>
       {data
         ? data.map((item, index) => (
-            <Grid container spacing={5} direction='row'>
+            <Grid container spacing={5} direction="row">
               <Grid item xs={4} spacing={5}>
                 {item.fieldName === "textfield" ? <TextField /> : null}
-              </Grid>
-              <Grid item xs={4} spacing={5}>
-                {item.fieldName === "checkbox" ? <Checkbox /> : null}
-              </Grid>
-              <Grid item xs={4} spacing={5}>
-                {item.fieldName === "select" ? <Select /> : null}
               </Grid>
               <Grid item xs={4} spacing={5}>
                 {item.fieldName === "textfield" ? <TextField /> : null}
               </Grid>
               <Grid item xs={4} spacing={5}>
-                {item.fieldName === "checkbox" ? <Checkbox /> : null}
+                {item.fieldName === "textfield" ? <TextField /> : null}
               </Grid>
               <Grid item xs={4} spacing={5}>
+                {item.fieldName === "textfield" ? <TextField /> : null}
+              </Grid>
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "textfield" ? <TextField /> : null}
+              </Grid>
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "textfield" ? <TextField /> : null}
+              </Grid>
+
+              <Grid item xs={4} spacing={5}>
                 {item.fieldName === "select" ? <Select /> : null}
+              </Grid>
+
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "select" ? <Select /> : null}
+              </Grid>
+
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "select" ? <Select /> : null}
+              </Grid>
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "select" ? (
+                  <TextField
+                    id="date"
+                    label="Birthday"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                ) : null}
+              </Grid>
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "select" ? (
+                  <TextField
+                    id="date"
+                    label="Birthday"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                ) : null}
+              </Grid>
+              <Grid item xs={4} spacing={5}>
+                {item.fieldName === "select" ? (
+                  <TextField
+                    id="date"
+                    label="Birthday"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                ) : null}
               </Grid>
             </Grid>
           ))
