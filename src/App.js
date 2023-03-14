804c28cee01bc80a07dffece358704df1cb8ddec
rohitthakur211 <112896810+rohitthakur211@users.noreply.github.com>
2023-03-14 18:07:28 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import "./App.css";
 import DynamicForm from "./component/DynamicForm";
 
+// const handleChange = () => {
+//   //handle textfield and select tag change logic here based on "NAME"
+// };
 
-const data = [
-  // { id: 'name', label: 'Name', value: '' },
-  // { id: 'email', label: 'Email', value: '' },
-  // { id: 'password', label: 'Password', value: '' },
-  // { id: 'phoneno', label: 'Phoneno', value: '' },
-  // { id: 'adress', label: 'adress', value: '' },
-  // { id: 'fathername', label: 'fathername', value: '' },
-  {
-    fieldName: "textfield",
-  },
-  {
-    fieldName: "checkbox",
-  },
-  {
-    fieldName: "select",
-  },
-];
 
 const App = (props) => {
   return (
     <div className="App">
-      <DynamicForm data={data} />
+      <DynamicForm  />
     </div>
   );
 };
